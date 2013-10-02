8c5ee224089989d42d7536e6d0de7e89f4b900be
Konstantin Sorokin <kvs@sigterm.ru>
2013-10-02 19:02:43 +0400

    test protobuf's serialization speed

--- test.cpp
+++ test.cpp
@@ -13,6 +13,8 @@
 #include "thrift/gen-cpp/test_types.h"
 #include "thrift/gen-cpp/test_constants.h"
 
+#include "protobuf/test.pb.h"
+
 const size_t      kItegersCount = 1000;
 const size_t      kStringsCount = 100;
 const int64_t     kIntegerValue = 26354;
@@ -53,10 +55,10 @@ thrift_serialization_test(size_t iterations)
     a2.read(protocol2.get());
 
     if (a != a2) {
-        throw std::logic_error("invariant failed");
+        throw std::logic_error("thrift's case: invariant failed");
     }
 
-    std::cout << "thrift: " << serialized.size() << " bytes" << std::endl;
+    std::cout << "thrift: serialized object size = " << serialized.size() << " bytes" << std::endl;
 
     auto start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < iterations; i++) {
@@ -67,23 +69,63 @@ thrift_serialization_test(size_t iterations)
     auto finish = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
 
-    std::cout << "thrift: " << duration << " milliseconds" << std::endl << std::endl;
+    std::cout << "thrift: serialization time = " << duration << " milliseconds" << std::endl << std::endl;
 }
 
 void
 protobuf_serialization_test(size_t iterations)
 {
+    using namespace protobuf_test;
+
+    Record a;
+
+    for (size_t i = 0; i < kItegersCount; i++) {
+        a.add_ids(kIntegerValue);
+    }
+
+    for (size_t i = 0; i < kStringsCount; i++) {
+        a.add_strings(kStringValue);
+    }
+
+    std::string serialized;
+
+    a.SerializeToString(&serialized);
+
+    // check if we can deserialize back
+    Record a2;
+    bool ok = a2.ParseFromString(serialized);
+    if (!ok /*|| a2 != a*/) {
+        throw std::logic_error("protobuf's case: invariant failed");
+    }
+
+    std::cout << "protobuf: serialized object size = " << serialized.size() << " bytes" << std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < iterations; i++) {
+        serialized.clear();
+        a.SerializeToString(&serialized);
+    }
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+
+    std::cout << "protobuf: serialization time = " << duration << " milliseconds" << std::endl << std::endl;
 }
 
 int
 main(int argc, char **argv)
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " iterations" << std::endl;
         exit(0);
     }
 
     auto iterations = boost::lexical_cast<size_t>(argv[1]);
+
+    std::cout << "performing " << iterations << " iterations" << std::endl << std::endl;
+
+    /*std::cout << "total size: " << sizeof(kIntegerValue) * kItegersCount + kStringValue.size() * kStringsCount << std::endl;*/
 
     thrift_serialization_test(iterations);
     protobuf_serialization_test(iterations);
